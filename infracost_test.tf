186239cf92775024f3b071d2a10b114e5843affc
cdupui <32101492+cdupui@users.noreply.github.com>
2025-04-09 15:52:43 +0200

    Update infracost_test.tf

--- infracost_test.tf
+++ infracost_test.tf
@@ -13,8 +13,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "6aa18476-1cde-4c21-868b-e0c5da8f588e"
-  tenant_id = "10000000-0000-0000-0000-000000000000"
 }
 
 # Create the very first resource
