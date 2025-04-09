df6ed78d5113209bdaa0af413e13cd0087de160d
cdupui <32101492+cdupui@users.noreply.github.com>
2025-04-09 14:05:44 +0200

    Update infracost_test.tf

--- infracost_test.tf
+++ infracost_test.tf
@@ -9,9 +9,12 @@ terraform {
 }
 
 # Configure the Microsoft Azure Provider
+
+
 provider "azurerm" {
   features {}
-  subscription_id = var.subscriptionId
+  subscription_id = "6aa18476-1cde-4c21-868b-e0c5da8f588e"
+  tenant_id = "10000000-0000-0000-0000-000000000000"
 }
 
 # Create the very first resource
